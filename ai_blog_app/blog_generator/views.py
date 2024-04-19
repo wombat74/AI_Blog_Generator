48646e3a8137dc81522caa689a4a00367a819e2b
Eddie G <eddie.gonzalez@gmail.com>
2024-04-18 17:08:01 -0700

    added assemblyai code

--- ai_blog_app/blog_generator/views.py
+++ ai_blog_app/blog_generator/views.py
@@ -3,13 +3,66 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.conf import settings
+import json
+from pytube import YouTube
+import os
+import assemblyai as aai
+
+def yt_title(link):
+    yt = YouTube(link)
+    title = yt.title
+    return title
+
+def download_audio(link):
+    yt = YouTube(link)
+    video = yt.strems.filter(only_audio=True).first()
+    out_file = video.download(output_path=settings.MEDIA_ROOT)
+    base, ext = os.path.splittext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+    return new_file
+
+def get_transcription(link):
+    audio_file = download_audio(link)
+    aai.settings.api_key = 'e82842ead23146819fc00b49b39a52c2'
+
+    transcriber = aai.Transcriber()
+    transcript = transcriber.transcribe(audio_file)
+
+    return transcript.text
 
 @login_required
 def index(request):
     return render(request, 'index.html')
 
+@csrf_exempt
 def generate_blog(request):
-    pass
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            yt_link = data['link']
+            return JsonResponse({'content': yt_link})
+        
+        except (KeyError, json.JSONDecodeError):
+            return JsonResponse({'error': 'Invalid data sent'}, status=400)
+        
+        # get yt title
+        title = yt_title(yt_link)
+        
+        # get yt transcript
+        transcription = get_transcription(yt_link)
+        if not transcription:
+            return JsonResponse({'error': 'Failed to get transcript'})
+        # use OpenAI to generate blog
+
+        # save blog article to db
+
+        # return blog article as a response
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
 
 def user_login(request):
     if request.method == 'POST':
